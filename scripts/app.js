8ea7ea3e3b0f7d5f3e33c5595a447db8e30b636c
SaifRehman99 <saifr7493@gmail.com>
2019-05-25 12:07:16 +0500

    search contact added

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,45 @@
-    
+    // getting the refrences here
+   
+    const input = document.querySelector('#inputContact');
+   
+    const listItem = document.querySelector('#contactsList');
+
+   
+    // adding event listener here
+
+    input.addEventListener('keyup',()=>{
+
+
+        // getting the input value here
+        const value = input.value.toLowerCase();
+
+
+        // getting the ul's li here array form
+        const li = listItem.querySelectorAll('li.collection-item');
+
+
+        // looping throung the li list
+        
+        li.forEach((item)=>{
+        
+            let a = item.getElementsByTagName('a')[0];
+
+
+            // if matched show or hide
+            
+            if(a.innerHTML.toLowerCase().indexOf(value) > -1){
+                item.style.display = '';
+            
+            }
+            
+            else{
+            
+                item.style.display = 'none';
+        
+            }
+        
+        })
+
+        
+
+    })
